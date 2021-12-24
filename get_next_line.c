cdcfc5a9669f287781661c047662203de20abba4
Cbukuba <chrisbukuba058@gmail.com>
2021-12-24 15:06:01 +0100

    V1

--- get_next_line.c
+++ get_next_line.c
@@ -6,44 +6,75 @@
 /*   By: cbukuba <cbukuba@student.s19.be>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/12/13 19:17:45 by cbukuba           #+#    #+#             */
-/*   Updated: 2021/12/22 00:48:21 by cbukuba          ###   ########.fr       */
+/*   Updated: 2021/12/24 15:05:07 by cbukuba          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
 #include <stdio.h>
 
+// int	count_line(char *str)
+// {
+// 	int	i;
+// 	int	j;
+
+// 	i = 0;
+// 	j = 0;
+// 	while (*str && str[i])
+// 	{
+// 		if (str[i] == '\n')
+// 			j ++;
+// 		i ++;
+// 	}
+// 	return (j);
+// }
+
 char	*get_next_line(int fd)
 {
 	char			buf[BUFFER_SIZE + 1];
-	int				ret = 1;
-	char			*temp = "";
+	int				ret;
 	static char		*str ="";
-	char			*line = "";
+	char			**box;
+	static int		i = -1;
+	// int				line_count = 0;
 
+	ret = 1;
 	while (ret > 0)
 	{
 		ret = read(fd, buf, BUFFER_SIZE);
-		printf("%d\n", ret);
 		buf[ret] = 0;
 		str = ft_strjoin(str, buf);
 		if (ft_strchr(str, '\n'))
 			break;
 	}
-	line = ft_strrchr(str, '\n');
-	temp = str;
-	str = ft_strchr(temp, '\n');
-	free (temp);
-	return (line);
+	// line_count = count_line(str);
+	// printf("%d", line_count);
+	box = ft_split(str, '\n');
+	// if (i < line_count)
+	i ++;
+	return (box[i]);
 }
 
 #include <sys/types.h>
 #include <sys/stat.h>
 #include <fcntl.h>
+// int main()
+// {
+// 	int fd;
+// 	fd = open("text.txt", O_RDONLY);
+// 	printf("%s\n", get_next_line(fd));
+// 	printf("%s", get_next_line(fd));
+// }
 int main()
 {
-	int fd;
-	fd = open("text.txt", O_RDONLY);
-	printf("%d\n", fd);
-	printf("%s", get_next_line(fd));
+    int fd;
+    char *line;
+    fd = open("text", O_RDONLY);
+
+    while (line != NULL)
+    {
+        line = get_next_line(fd);
+        printf("--- FINAL LINE = %s\n", line);
+    }
+    return (0);
 }
--- get_next_line.h
+++ get_next_line.h
@@ -10,6 +10,7 @@ char	*ft_strjoin(char const *s1, char const *s2);
 char	*ft_strtrim(char const *s1, char const *set);
 char	*ft_substr(char const *s, unsigned int start, size_t len);
 char	*ft_strrchr(const char *s, int c);
+char	**ft_split(char const *s, char c);
 size_t	ft_strlen(char const *str);
 char	*get_next_line(int fd);
 #endif
