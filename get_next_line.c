dcbfd16208a0ff3bf448eeab928c186bd067816c
Cbukuba <chrisbukuba058@gmail.com>
2022-01-24 23:21:40 +0100

    next_line

--- get_next_line.c
+++ get_next_line.c
@@ -6,16 +6,32 @@
 /*   By: cbukuba <cbukuba@student.s19.be>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/12/13 19:17:45 by cbukuba           #+#    #+#             */
-<<<<<<< HEAD
-/*   Updated: 2022/01/21 03:16:29 by cbukuba          ###   ########.fr       */
-=======
-/*   Updated: 2022/01/24 17:16:20 by cbukuba          ###   ########.fr       */
->>>>>>> f12a2051b63f48c8e2362bad0c0a618e99048c43
+/*   Updated: 2022/01/24 21:46:44 by cbukuba          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
 #include <stdio.h>
+
+static char	*next_line(char *str)
+{
+	char	*next_line;
+	int		i;
+	// int		j;
+
+	i = 0;
+	// j = 0;
+	while (!(ft_strchr(str, '\n')) && str[i])
+		i ++;
+	// next_line = malloc(sizeof(char) * ft_strlen(str) - i + 1);
+	next_line = ft_substr(str, i, ft_strlen(str) - i);
+	// if (!next_line)
+	// 	return (NULL);
+	// while (str[i])
+	// 	next_line[j ++] = str[i ++];
+	free (str);
+	return (next_line);
+}
 
 char	*get_next_line(int fd)
 {
@@ -23,33 +39,26 @@ char	*get_next_line(int fd)
 	int				ret;
 	char			*line;
 	static char		*str = "";
+	int				i;
 
+	i = 0;
 	ret = 1;
-	if (fd == -1)
+	if (fd < 0)
 		return (NULL);
 	while (ret > 0)
 	{
-<<<<<<< HEAD
 		ret = read(fd, buf, 1);
-=======
-		ret = read(fd, buf, BUFFER_SIZE);
-		if (ret < 0)
+		if (ret == -1)
 			return (NULL);
 		buf[ret] = 0;
->>>>>>> f12a2051b63f48c8e2362bad0c0a618e99048c43
 		str = ft_strjoin(str, buf);
 		if (ft_strchr(str, '\n'))
+			i ++;
+		if (i == 2)
 			break;
-		buf[ret] = 0;
 	}
-<<<<<<< HEAD
-	if (str[ft_strlen(str)] == '\0')
-		return (NULL);
 	line = ft_strdup(str);
-	str = str + (ft_strlen(str) + 1);
-=======
-	line = ft_strdup(str);
->>>>>>> f12a2051b63f48c8e2362bad0c0a618e99048c43
+	str = next_line(str);
 	return (line);
 }
 
@@ -66,12 +75,8 @@ int main()
 	// {
 		line = get_next_line(fd);
 		printf("%s\n", line);
-<<<<<<< HEAD
-	}
-=======
 		line = get_next_line(fd);
-		printf("%s\n", line);
+		printf("%s", line);
 	// }
->>>>>>> f12a2051b63f48c8e2362bad0c0a618e99048c43
 	return 0;
 }
--- get_next_line_utils.c
+++ get_next_line_utils.c
@@ -10,6 +10,23 @@ size_t	ft_strlen(char const *str)
 	return (c);
 }
 
+static	char	*ft_strcat(char *dest, char const *src)
+{
+	int	i;
+	int	j;
+
+	i = 0;
+	while (dest[i] != '\0')
+		i ++;
+	j = 0;
+	while (src[j] != '\0')
+	{
+		dest[i + j] = src[j];
+		j++;
+	}
+	return (dest);
+}
+
 char	*ft_strchr(char const *s, int c)
 {
 	int		i;
@@ -26,23 +43,6 @@ char	*ft_strchr(char const *s, int c)
 	if (str[i] == (char)c)
 		return (str + i);
 	return (NULL);
-}
-
-static	char	*ft_strcat(char *dest, char const *src)
-{
-	int	i;
-	int	j;
-
-	i = 0;
-	while (dest[i] != '\0')
-		i ++;
-	j = 0;
-	while (src[j] != '\0')
-	{
-		dest[i + j] = src[j];
-		j++;
-	}
-	return (dest);
 }
 
 static char	*str_link(char *str, char const *s1, char const *s2)
@@ -81,61 +81,6 @@ char	*ft_strjoin(char const *s1, char const *s2)
 	*str = 0;
 	str_link(str, s1, s2);
 	return (str);
-}
-
-static int	find_in_set(char c, char const *set)
-{
-	int	i;
-
-	i = 0;
-	while (set[i])
-	{
-		if (set[i] == c)
-			return (1);
-		i ++;
-	}
-	return (0);
-}
-
-char	*ft_strtrim(char const *s1, char const *set)
-{
-	char	*str;
-	int		i;
-	int		start;
-	int		len;
-
-	start = 0;
-	while (find_in_set(s1[start], set) && s1[start])
-		start ++;
-	len = ft_strlen(s1);
-	while (find_in_set(s1[len - 1], set) && len > start)
-		len --;
-	str = malloc(sizeof(char) * (len - start + 1));
-	if (!str)
-		return (NULL);
-	i = 0;
-	while (start < len)
-		str[i ++] = s1[start ++];
-	str[i] = '\0';
-	return (str);
-}
-
-char	*ft_strrchr(const char *s, int c)
-{
-	int		i;
-	char	*str;
-
-	str = (char *)s;
-	i = 0;
-	while (str[i])
-		i ++;
-	while (i >= 0)
-	{
-		if (str[i] == (char)c)
-			return (str + i);
-		i --;
-	}
-	return (NULL);
 }
 
 static	int	word_count(char const *s, char c)
