f12a2051b63f48c8e2362bad0c0a618e99048c43
Cbukuba <cbukuba@student.s19.be>
2022-01-24 17:25:03 +0100

    il faut save

--- get_next_line.c
+++ get_next_line.c
@@ -6,78 +6,52 @@
 /*   By: cbukuba <cbukuba@student.s19.be>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/12/13 19:17:45 by cbukuba           #+#    #+#             */
-/*   Updated: 2022/01/21 03:06:21 by cbukuba          ###   ########.fr       */
+/*   Updated: 2022/01/24 17:16:20 by cbukuba          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
-
-static int	count_line(char *str)
-{
-	int	i;
-	int	j;
-
-	i = 0;
-	j = 0;
-	while (*str && str[i])
-	{
-		if (str[i] == '\n')
-			j ++;
-		i ++;
-	}
-	return (j);
-}
-
-static	char	**free_tab(char **tab)
-{
-	int	i;
-
-	i = 0;
-	while (tab[i])
-	{
-		free(tab[i]);
-		i ++;
-	}
-	free (tab);
-}
+#include <stdio.h>
 
 char	*get_next_line(int fd)
 {
 	char			buf[BUFFER_SIZE + 1];
 	int				ret;
 	char			*line;
-	static char		*str ="";
+	static char		*str = "";
 
 	ret = 1;
+	if (fd == -1)
+		return (NULL);
 	while (ret > 0)
 	{
-		ret = read(fd, buf, 1);
+		ret = read(fd, buf, BUFFER_SIZE);
+		if (ret < 0)
+			return (NULL);
 		buf[ret] = 0;
 		str = ft_strjoin(str, buf);
 		if (ft_strchr(str, '\n'))
 			break;
 	}
 	line = ft_strdup(str);
-	if (str[ft_strlen(str) - 1] == '\0')
-		return (NULL);
-	str = str + (ft_strlen(str) + 1);
 	return (line);
 }
 
 #include <sys/types.h>
 #include <sys/stat.h>
 #include <fcntl.h>
-#include <stdio.h>
 int main()
 {
 	int fd;
 	fd = open("text.txt", O_RDONLY);
 	char *line = "";
 
-	while (line != NULL)
-	{
+	// while (line != NULL)
+	// {
 		line = get_next_line(fd);
-		printf("%s", line);
-	}
+		printf("%s\n", line);
+		line = get_next_line(fd);
+		printf("%s\n", line);
+	// }
 	return 0;
 }
