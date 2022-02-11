e619a5313cd67d8cbe1ecaf23f3389d3de771e4d
Cbukuba <chrisbukuba058@gmail.com>
2022-02-11 20:23:41 +0100

    Dernière ligne

--- get_next_line.c
+++ get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: cbukuba <cbukuba@student.s19.be>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/12/13 19:17:45 by cbukuba           #+#    #+#             */
-/*   Updated: 2022/02/10 18:08:45 by cbukuba          ###   ########.fr       */
+/*   Updated: 2022/02/11 19:53:49 by cbukuba          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,34 +19,35 @@ static char	*next_line(char *str)
 	int		i;
 
 	i = 0;
-	while ((ft_strchr(str, '\n')) && str[i])
+	while (str[i] != '\n' && str[i] != 0)
 		i ++;
-	next_line = malloc(sizeof(char) * (ft_strlen(str) - i + 1));
-	if (!next_line)
-		return(NULL);
-	int j = 0;
-	while (str[i])
-		next_line[j++] = str[i ++];
-	next_line = 0;
+	if (str[i] == 0)
+		return (NULL);
+	next_line = ft_substr(str, i + 1, ft_strlen(str) - i);
+	if (next_line[0] == '\0')
+	{
+		free(next_line);
+		return (NULL);
+	}
+	str[i] = 0;
 	return (next_line);
-	// ft_substr(str, i, ft_strlen(str))
 }
 
 static	char *first_line(char *str)
 {
-	int	i;
-	char *first_line;
+	int		i;
+	char	*first_line;
 
 	i = 0;
 	if (!str)
 		return (NULL);
-	while ((ft_strchr(str, '\n')) && str[i])
+	while (str[i] != '\n' && str[i])
 		i ++;
-	first_line = malloc(sizeof(char) * (i + 2));
+	first_line = malloc(sizeof(char) * (i + 1));
 	if (!first_line)
 		return(NULL);
 	i = 0;
-	while (str[i] && str[i] != '\n')
+	while ( str && str[i] && str[i] != '\n')
 	{
 		first_line[i] = str[i];
 		i ++;
@@ -65,20 +66,22 @@ char	*get_next_line(int fd)
 	char			buf[BUFFER_SIZE + 1];
 	int				ret;
 	char			*line;
-	static char		*str = "";
+	static char		*str;
 	
 	if (fd < 0)
 		return (NULL);
-	ret = read(fd, buf, BUFFER_SIZE);
+	ret = 1;
 	while (ret > 0)
 	{
+		ret = read(fd, buf, BUFFER_SIZE);
 		if (ret == -1)
 			return (NULL);
 		buf[ret] = 0;
+		if (!str)
+			str = ft_strdup("");
 		str = ft_strjoin(buf, str);
 		if (ft_strchr(str, '\n'))
 			break;
-		ret = read(fd, buf, BUFFER_SIZE);
 	}
 	line = first_line(str);
 	str = next_line(str);
@@ -94,12 +97,10 @@ int main()
 	fd = open("text.txt", O_RDONLY);
 	char *line = "";
 
-	// while (line)
-	//  {
+	while (line)
+	 {
 		line = get_next_line(fd);
 		printf("%s", line);
-		line = get_next_line(fd);
-		printf("%s", line);
-	//  }
+	 }
 	return 0;
 }
